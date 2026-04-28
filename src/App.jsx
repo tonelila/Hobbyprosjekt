d118ce6f8120aab73fdc03cfcbb55dc21ed577bb
Tone-Lill Aarsheim <tonelila@stud.ntnu.no>
2026-04-28 18:18:07 +0200

    hei sarah

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="card">
-      <h1>Velkommen! 👋</h1>
+      <h1>Velkommen Sarah! 👋</h1>
       <p>
         Du kan se koden for dette prosjektet{" "}
         <a href="https://github.com/bekk/prosjektutgangspunkt">
